509e1df1d7e9dcdcf6df74f278b2020f74e9316c
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2025-10-22 15:20:21 +0200

    #8 The Last Project Installment Steps
    I did the last steps of installing 11ty projects what is called Advance Features:
    
    - Custom Data
    - Loops
    - Filters
    - Fetch Data from API
    - Linking the project to Netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addFilter("randomItem", (arr) => {
+  arr.sort(() => {
+    return 0.5 - Math.random();
+    });
+    return arr.slice(0, 1);
+  });
+
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css/");
   return {
@@ -8,3 +16,4 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+
